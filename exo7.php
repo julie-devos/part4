0c6d7c15cf52cef0da16b8ac10cfa8ba559bfdcd
julie-devos <julie.devos@epitech.eu>
2020-12-08 15:58:19 +0100

    initial commit

--- exo7.php
+++ exo7.php
@@ -6,7 +6,25 @@
 </head>
 <body>
 <?php
-
+$age=rand(1,100);
+$genderArray= array("Homme", "Femme");
+$genderRand= array_rand($genderArray);
+$gender= $genderArray[$genderRand];
+function phrase($age,$gender){
+    if ($age<18 && $gender=="Homme"){
+        echo "Vous êtes un homme et vous êtes mineur.";
+    }
+    elseif ($age>=18 && $gender=="Homme"){
+        echo "Vous êtes un homme et vous êtes majeur.";
+    }
+    elseif ($age<18 && $gender=="Femme"){
+        echo "Vous êtes une femme et vous êtes mineur.";
+    }
+    else {
+        echo "Vous êtes une femme et vous êtes majeur.";
+    }
+}
+phrase($age,$gender);
 ?>
 
 </body>
