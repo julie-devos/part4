2706a6b9f1b564333b434d423b54554df83b5a06
julie-devos <julie.devos@epitech.eu>
2020-12-08 15:47:49 +0100

    initial commit

--- exo6.php
+++ exo6.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Exercice 5-Partie 4</title>
+    <title>Exercice 6-Partie 4</title>
 </head>
 <body>
 <?php
---
+++ exo7.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 7-Partie 4</title>
+</head>
+<body>
+<?php
+
+?>
+
+</body>
+</html>
