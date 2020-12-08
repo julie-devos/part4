84bb2ce88e8f2b4667e8d98480b8cecb8db38892
julie-devos <julie.devos@epitech.eu>
2020-12-08 14:52:09 +0100

    initial commit

---
+++ exo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 1-Partie 4</title>
+</head>
+<body>
+<?php
+
+function boolean(){
+    $i=true;
+    ?>
+<?=$i?>
+<?php
+}
+boolean();
+?>
+
+</body>
+</html>
---
+++ exo2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 2-Partie 4</title>
+</head>
+<body>
+<?php
+
+function phrase(){?>
+    <p><?="Bonjour toi, je suis une chaîne de caractères envoyée par une fonction."?></p>
+
+  <?php
+}
+phrase();
+?>
+
+
+</body>
+</html>
