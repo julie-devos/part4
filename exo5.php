b214effc1cafc48eef268344d2c03bfe7de43358
julie-devos <julie.devos@epitech.eu>
2020-12-08 15:43:40 +0100

    initial commit

---
+++ exo5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 5-Partie 4</title>
+</head>
+<body>
+<?php
+
+function phrase($name,$age){?>
+    <p><?="Bonjour $name, tu as $age ans !"?></p>
+
+    <?php
+}
+phrase("Julie",27);
+?>
+
+</body>
+</html>
